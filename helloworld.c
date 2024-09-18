c68d8a5999e4b8608f07132bf8e70d226a78f199
Anusha C <anushachandru24@gmail.com>
2024-09-18 14:58:06 +0530

    input user name

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main(){
-  int num1;
-  printf("Enter num: ");
-  scanf("%d\n", &num1);
-  printf("Hello World %d\n", num1);
+  char name[70];
+  scanf("%s\n", &name);
+  printf("Hello %s\n", name);
 }
