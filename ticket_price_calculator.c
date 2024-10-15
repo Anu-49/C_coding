b936d807efe15f6a29cbf48990380513397ad5ff
Anusha C <anushachandru24@gmail.com>
2024-10-15 21:28:03 +0530

    Program to calculate ticket price with discount based on age

--- ticket_price_calculator.c
+++ ticket_price_calculator.c
@@ -11,15 +11,15 @@ int main(){
         printf("Children under 5 years old get in free!\n");
         printf("Ticket price: Rs. %.2f", tp * 0);
     }
-    // 50% discount offer for children between age 5 & 12
+    // 50% discount offer for children between age 5 & 12.
     else if(age>=5 && age<=12){
         printf("Ticket price: Rs. %.2f", tp*0.5);
     }
-    // NO discount for people between age 12 & 65
+    // NO discount for people between age 12 & 65.
     else if(age>12 && age<65){
         printf("Ticket price: Rs. %.2f", tp);
     }
-    // 20% discount offer for elderly people of age 65 and above
+    // 20% discount offer for elderly people of age 65 and above.
     else if (age>=65){
         printf("Ticket price: Rs. %.2f", tp*0.8);
     }
