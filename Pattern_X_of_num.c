449b3a2d1cab907219c0b19b8ebd216bcff0c518
Anusha C <anushachandru24@gmail.com>
2024-11-07 21:20:59 +0530

    program to print numbers in X pattern

---
+++ Pattern_X_of_num.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int num;
+    // read the number of rows
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    /* based on number of rows print the numbers in 'X' pattern i.e, 
+  => if num = 6: 
+    1    6
+     2  5
+      34 
+      34
+     2  5
+    1    6 
+  => if num = 5
+    1   5
+     2 4
+      3
+     2 4
+    1   5     */
+    for(int i=1; i<=num; i++){
+        for(int j=1; j<=num; j++){
+            if(i==j || i == (num+1)-j){
+                printf("%d", j);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
